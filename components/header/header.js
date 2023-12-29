2971c3a2dc2c6e15284e22c8e860329929076a3f
oucheoof <stephendoan@hotmail.com>
2023-12-29 22:49:54 +0100

    Header fixed

--- components/header/header.js
+++ components/header/header.js
@@ -1,16 +1,31 @@
-
-
 function render_header ()
 {
-  // Funktionen tar emot referensen till container-noden för headern.
-  // Funktionsanropet finns redan på plats i index.js. Studera argumentet som skickas där.
-  // Funktionen skapar de nödvändiga HTML-elementen så att headern ser ut
-  // som den på videon: Tre random bilder och texten centrerad.
+  let newHeader = document.createElement("header");
+  let body = document.querySelector("body");
 
-  // Krav:
-  // Nyckeln *style* får i denna kod endast användas för att ange bakgrundsbild-filen.
-  // Det går dock såklart även att lösa det hela med <img> istället för att ha bilderna
-  // som bakgrundsbilder. I så fall får inte nyckeln *style* användas alls i koden här.
+  let newImg1 = document.createElement("img");
+  let newImg2 = document.createElement("img");
+  let newImg3 = document.createElement("img");
 
-  // Glöm inte: Du måste även fylla i header.css så att headern ser ut som den på videon. Se header.css
+
+  let textContainer = document.createElement("div");
+  let newH1Text = document.createElement("h1");
+  let newH1Text2 = document.createElement("h1");
+
+  newH1Text.textContent = "Utbytesstudier"
+  newH1Text2.textContent = "Missa inte chansen!"
+
+  body.appendChild(newHeader);
+  newHeader.appendChild(newImg1);
+  newHeader.appendChild(textContainer)
+  textContainer.appendChild(newH1Text);
+  textContainer.appendChild(newH1Text2);
+  newHeader.appendChild(newImg2);
+  newHeader.appendChild(newImg3);
+
+
+  
+  newImg1.src = "media/geo_images/argentina_normal_1.jpg";
+  newImg2.src = "media/geo_images/argentina_normal_2.jpg";
+  newImg3.src = "media/geo_images/argentina_normal_3.jpg";
 }
