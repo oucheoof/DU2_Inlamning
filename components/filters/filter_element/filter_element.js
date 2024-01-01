8b338c0513267ac9fc9c254cc4653b8554772929
oucheoof <stephendoan@hotmail.com>
2024-01-01 21:14:52 +0100

    Kommentarer för att förstå kod.

--- components/filters/filter_element/filter_element.js
+++ components/filters/filter_element/filter_element.js
@@ -1,19 +1,22 @@
-function render_filter_element (parent, text)
+function render_filter_element (parent, text) // Parent är där det nya filterelementet ska läggas till, text är namnet på filteret. Ex. från language_filters (container, language.name).
 {
-  const container = document.createElement("div");
-  parent.append(container);
-  container.classList.add("filter_element");
+  const container = document.createElement("div"); //Ny div skapas
+  parent.append(container); //Lägger till det i vår parent från functions anorpet ex. container från language_filters. 
+  container.classList.add("filter_element"); //Ger den nya diven en class filter_element
 
   container.innerHTML = `
     <div class="check_container">
       <div class="check_mark"></div>
     </div>
     <div class="text">${text}</div>
-  `;
+  `; // Skapar en div som ska gå att klicka på med texten från funktions anropet vilket är language.name i vårt fall. 
 
   container.addEventListener("click", function () 
   {
-    container.classList.toggle("checked");    
-    update_programme_list();
+    container.classList.toggle("checked"); //Toggelar mellan checked och ingen check class för att ta bort och slå på CSS regler. 
+    update_programme_list(); //Funktion som uppdaterar listan
   });
+  //När någon klickar på container så körs koden. När man klickar på den nya diven så ändrar vi 
 }
+
+//TLDR: Skapar checkboxen för w/e filter som kallar på funktionen render_filter_element
--- components/filters/language_filters/language_filters.js
+++ components/filters/language_filters/language_filters.js
@@ -1,12 +1,33 @@
 
 function render_language_filters (parent)
 {
-  const container = document.createElement("div");
-  container.id = "language_filters";
-  parent.append(container);
+  const container = document.createElement("div"); //Skapar en ny div i vår parent som vi väljer (i index.js) (ta bort)
+  container.id = "language_filters"; //Ger id till den nya diven (ta bort)
+  parent.append(container); //Lägger till den nya diven (container) till vår förälder vilket är det vi väljer i (index.js)
   
-  for (let language of LANGUAGES)
+  for (let language of LANGUAGES) // Går i genom hela LANGUAGES databas
   {
     render_filter_element(container, language.name);
   }
+  // För att förstå ovan behöver du förstå:
+
+  /* function render_filter_element (parent, text)
+{
+  const container = document.createElement("div");
+  parent.append(container);
+  container.classList.add("filter_element");
+
+  container.innerHTML = `
+    <div class="check_container">
+      <div class="check_mark"></div>
+    </div>
+    <div class="text">${text}</div>
+  `;
+
+  container.addEventListener("click", function () 
+  {
+    container.classList.toggle("checked");    
+    update_programme_list();
+  });
+} */
 }
