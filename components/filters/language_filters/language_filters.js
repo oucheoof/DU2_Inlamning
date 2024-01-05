5e3f3c912a84256796591c23a51faf2812ced34e
oucheoof <stephendoan@hotmail.com>
2024-01-05 21:33:06 +0100

    Update language_filters.js

--- components/filters/language_filters/language_filters.js
+++ components/filters/language_filters/language_filters.js
@@ -1,15 +1,15 @@
 
 function render_language_filters (parent)
 {
-  const container = document.createElement("div"); //Skapar en ny div i vår parent som vi väljer (i index.js) (ta bort)
-  container.id = "language_filters"; //Ger id till den nya diven (ta bort)
-  parent.append(container); //Lägger till den nya diven (container) till vår förälder vilket är det vi väljer i (index.js)
+  const container = document.createElement("div"); 
+  container.id = "language_filters"; 
+  parent.append(container); 
   
-  for (let language of LANGUAGES) // Går i genom hela LANGUAGES databas
+  for (let language of LANGUAGES) 
   {
-    render_filter_element(container, language.name); //För varje land ska en checkbox skapas
+    render_filter_element(container, language.name); 
   }
-  // För att förstå ovan behöver du förstå: filter_element.js
+
 
   
 }
